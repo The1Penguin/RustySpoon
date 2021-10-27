6a2167e7e359e1453179b8d73c8ae161d5a3adb0
thepenguin <jesper@acorneroftheweb.com>
2021-10-27 21:39:27 +0200

    Made the bot a bit more moddable

--- src/commands/sentinel.rs
+++ src/commands/sentinel.rs
@@ -1,10 +1,34 @@
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
     prelude::*,
+    framework::standard::{
+        buckets::{LimitedFor, RevertBucket},
+        help_commands,
+        macros::{check, command, group, help, hook},
+        Args,
+        CommandGroup,
+        CommandOptions,
+        CommandResult,
+        DispatchError,
+        HelpOptions,
+        Reason,
+        StandardFramework,
+    },
+    http::Http,
+    model::{
+        channel::{Channel, Message},
+        gateway::Ready,
+        id::UserId,
+        permissions::Permissions,
+    },
 };
 
 #[command]
-async fn temp (ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+#[allowed_roles("Sentinels")]
+pub async fn temp (ctx: &Context, msg: &Message, _args: Args) -> CommandResult {
+    if let Err(why) = msg.channel_id.say(&ctx.http, "Pong!").await {
+                println!("Error sending message: {:?}", why);
+                ()
+            }
     Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
-use std::env;
+mod commands;
+
+use crate::commands::sentinel::TEMP_COMMAND;
+
+use std::{
+    collections::{HashMap, HashSet},
+    env,
+    fmt::Write,
+    sync::Arc,
+};
 
 use serenity::{
     async_trait,
-    model::{channel::Message, gateway::Ready},
     prelude::*,
+    framework::standard::{
+        buckets::{LimitedFor, RevertBucket},
+        help_commands,
+        macros::{check, command, group, help, hook},
+        Args,
+        CommandGroup,
+        CommandOptions,
+        CommandResult,
+        DispatchError,
+        HelpOptions,
+        Reason,
+        StandardFramework,
+    },
+    http::Http,
+    model::{
+        channel::{Channel, Message},
+        gateway::Ready,
+        id::UserId,
+        permissions::Permissions,
+    },
 };
 
 struct Handler;
+
+#[group]
+#[commands(temp)]
+struct Sentinel;
 
 #[async_trait]
 impl EventHandler for Handler {
@@ -24,9 +56,39 @@ async fn main() {
     // Extract token from env variable
     let token = env::var("DISCORD_TOKEN").expect("Expected a token in the environment");
 
+        let http = Http::new_with_token(&token);
+
+    // We will fetch your bot's owners and id
+    let (owners, bot_id) = match http.get_current_application_info().await {
+        Ok(info) => {
+            let mut owners = HashSet::new();
+            if let Some(team) = info.team {
+                owners.insert(team.owner_user_id);
+            } else {
+                owners.insert(info.owner.id);
+            }
+            match http.get_current_user().await {
+                Ok(bot_id) => (owners, bot_id.id),
+                Err(why) => panic!("Could not access the bot id: {:?}", why),
+            }
+        },
+        Err(why) => panic!("Could not access application info: {:?}", why),
+    };
+
+
+    let framework = StandardFramework::new()
+        .configure(|c| c
+                        .prefix("~")
+                        .owners(owners))
+                        .group(&SENTINEL_GROUP);
+
     // Creates a client and a handler
     let mut client =
-        Client::builder(&token).event_handler(Handler).await.expect("Err creating client");
+        Client::builder(&token)
+            .framework(framework)
+            .event_handler(Handler)
+            .await
+            .expect("Err creating client");
 
     // Starts the client
     if let Err(why) = client.start().await {
